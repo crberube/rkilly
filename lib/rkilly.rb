25a2eadd9ebad1d54e51fb04ab0873ee0c2eee6f
Christopher Berube <cberube@adharmonics.com>
2014-07-21 14:28:55 -0400

    initial gem creation

---
+++ lib/rkilly.rb
@@ -0,0 +1,38 @@
+module Rkilly
+  require 'rubygems'
+  require 'commander/import'
+
+  program :version, '0.0.1'
+  program :description, 'Easily kill processes'
+ 
+  command :kill do |c|
+    c.syntax = 'kill [process name]'
+    c.summary = 'Easily kill a process'
+    c.description = 'Finds all processes that match [process name] and allows you to kill any specific match or all matches.'
+    c.action do |args, options|
+      pname = args[0]
+      data_array = %x(ps aux | grep #{pname} | grep -v grep | awk '{print $2, $11, $12, $13}').split("\n").drop(1)
+    
+      container_array = []
+      data_array.each do |cols|
+        elements = cols.split(' ')
+        elements = [elements.first, elements[1...elements.size].join(' ')]
+        elements.each_slice(2) { |slice| container_array << slice }
+      end
+
+      choose 'Which would you like to kill?' do |menu|
+        container_array.map { |e| e.last }.each_with_index do |choice, index|
+          menu.choice(choice) do
+            %x(kill -9 #{container_array[index].first.to_i})
+          end
+        end
+
+        menu.choice('All') do
+          container_array.map { |e| e.first }.each do |pid|
+            %x(kill -9 #{pid.to_i})
+          end
+        end
+      end
+    end
+  end
+end
---
+++ rkilly.gemspec
@@ -0,0 +1,15 @@
+Gem::Specification.new do |s|
+  s.name        = "rkilly"
+  s.version     = "0.0.1"
+  s.summary     = "Easily kill processes"
+  s.date        = "2014-07-21"
+  s.description = "Provides a kill command which finds all processes that match the provided argument and allows you to kill any specific match or all matches."
+  s.authors     = ["Christopher Berube"]
+  s.email       = ["cberube@adharmonics.com"]
+  s.license     = "MIT"
+  s.homepage    = "https://github.com/crberube/rkilly"
+  s.files       = ["lib/rkilly.rb"]
+  s.executables = "rkilly"
+  
+  s.add_runtime_dependency("commander", "~> 4.2")
+end
